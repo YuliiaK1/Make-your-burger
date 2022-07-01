4a0aa26dd0be7b135b163be2daffa33898837cd4
Yuliia Kulachko <svistaky@gmail.com>
2022-07-01 15:49:24 +0300

    Add Footer Component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from './Header';
+import Main from './Main';
+import Section from './Section';
 import Footer from './Footer';
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <div className="wrapper">
       <Header/>
       <div className='main-section'>
+      <Main />
+      <Section />
       </div>
       <Footer />
     </div>
