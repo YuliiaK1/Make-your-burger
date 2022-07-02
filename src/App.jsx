748f34cb370d99bb78b5ecbbc56ac704caa7eba5
Yuliia Kulachko <svistaky@gmail.com>
2022-07-02 10:51:20 +0300

    Modified js in jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import Header from './Header';
 import Main from './Main';
-import Section from './Section';
 import Footer from './Footer';
+
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
-      <div className='main-section'>
       <Main />
-      <Section />
-      </div>
       <Footer />
     </div>
   );
---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import Section from './Section';
+import './Main.scss';
+function Main() {
+    return (
+      <div className="main-section">
+        <div className='main'>
+          <h1>Make Your Burger</h1>
+        </div>
+        <Section />
+      </div>
+  );
+}
+
+export default Main;
