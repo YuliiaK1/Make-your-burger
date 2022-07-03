a43ab37025a717b07a6ac2a755a2ddf9ce34d4c2
Yuliia Kulachko <svistaky@gmail.com>
2022-07-03 16:27:44 +0300

    Add disabled functional on button count.

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,20 +1,31 @@
+import React from 'react';
 import './Ingredients.scss';
 
-
 function Ingredients(props) {
+  const [count, setCount] = React.useState(0);
+  
+  const plus = () => {
+    setCount(count+1);
+  };
+
+  const minus = () => {
+    setCount(count-1);
+  };
+
+
     return (
       <div className="ingredients">
         <div className="ingr-card">
             <div className="image-ingr">
-              <img src={props.image} alt="cotlet" />
+              <img src={props.image} alt="cotlet" /> 
             </div>
             <div className="name-ingr">
               <p>{props.name}</p>
             </div>
             <div className="counter">
-              <button>-</button>
-              <p>0</p>
-              <button>+</button>
+              <button disabled={!count} onClick={minus}>-</button>
+              <p>{count}</p>
+              <button onClick={plus}>+</button>
             </div>
       </div>
       </div>
